15e6ac203b396d92db2f2d66c6248d8e06034ec3
Ichika354 <handa82624@gmail.com>
2024-10-19 00:18:54 +0700

    Update Market

--- controller/menu.go
+++ controller/menu.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 	"github.com/gocroot/helper/atdb"
 	"github.com/gocroot/helper/watoken"
 	"github.com/gocroot/model"
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -195,6 +197,143 @@ func CreateToko(respw http.ResponseWriter, req *http.Request) {
 	// Response sukses dengan data lengkap
 	at.WriteJSON(respw, http.StatusOK, response)
 }
+
+func UpdateToko(respw http.ResponseWriter, req *http.Request) {
+	// Validasi token
+	payload, err := watoken.Decode(config.PublicKeyWhatsAuth, at.GetLoginFromHeader(req))
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error: Token Tidak Valid"
+		respn.Info = at.GetSecretFromHeader(req)
+		respn.Location = "Decode Token Error"
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusForbidden, respn)
+		return
+	}
+
+	// Ambil ID toko dari parameter URL
+	vars := mux.Vars(req) // Menggunakan Gorilla Mux
+	tokoID := vars["id"]
+	if tokoID == "" {
+		var respn model.Response
+		respn.Status = "Error: ID Toko tidak ditemukan"
+		at.WriteJSON(respw, http.StatusBadRequest, respn)
+		return
+	}
+
+	// Decode body request menjadi struct Toko
+	var tokoInput model.Toko
+	err = json.NewDecoder(req.Body).Decode(&tokoInput)
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error: Body tidak valid"
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusBadRequest, respn)
+		return
+	}
+
+	// Cek apakah toko dengan ID yang diberikan ada
+	objectID, err := primitive.ObjectIDFromHex(tokoID)
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error: ID Toko tidak valid"
+		at.WriteJSON(respw, http.StatusBadRequest, respn)
+		return
+	}
+
+	var existingToko model.Toko
+	filter := bson.M{"_id": objectID}
+	err = config.Mongoconn.Collection("menu").FindOne(context.TODO(), filter).Decode(&existingToko)
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error: Toko tidak ditemukan"
+		at.WriteJSON(respw, http.StatusNotFound, respn)
+		return
+	}
+
+	// Cek apakah user yang ingin mengupdate toko adalah pemilik toko
+	if existingToko.User[0].PhoneNumber != payload.Id {
+		var respn model.Response
+		respn.Status = "Error: User tidak memiliki hak akses untuk mengupdate toko ini"
+		at.WriteJSON(respw, http.StatusForbidden, respn)
+		return
+	}
+
+	// Cek apakah nama toko sudah digunakan oleh toko lain
+	if tokoInput.NamaToko != "" && tokoInput.NamaToko != existingToko.NamaToko {
+		var tokoNama model.Toko
+		err := config.Mongoconn.Collection("menu").FindOne(context.TODO(), bson.M{"nama_toko": tokoInput.NamaToko}).Decode(&tokoNama)
+		if err == nil {
+			var respn model.Response
+			respn.Status = "Error: Nama Toko sudah digunakan"
+			at.WriteJSON(respw, http.StatusBadRequest, respn)
+			return
+		}
+	}
+
+	// Cek apakah slug toko sudah digunakan oleh toko lain
+	if tokoInput.Slug != "" && tokoInput.Slug != existingToko.Slug {
+		var tokoSlug model.Toko
+		err := config.Mongoconn.Collection("menu").FindOne(context.TODO(), bson.M{"slug": tokoInput.Slug}).Decode(&tokoSlug)
+		if err == nil {
+			var respn model.Response
+			respn.Status = "Error: Slug Toko sudah digunakan"
+			at.WriteJSON(respw, http.StatusBadRequest, respn)
+			return
+		}
+	}
+
+	// Update data toko
+	updateData := bson.M{}
+	if tokoInput.NamaToko != "" {
+		updateData["nama_toko"] = tokoInput.NamaToko
+	}
+	if tokoInput.Slug != "" {
+		updateData["slug"] = tokoInput.Slug
+	}
+	if tokoInput.Alamat.Street != "" {
+		updateData["alamat.street"] = tokoInput.Alamat.Street
+	}
+	if tokoInput.Alamat.Province != "" {
+		updateData["alamat.province"] = tokoInput.Alamat.Province
+	}
+	if tokoInput.Alamat.City != "" {
+		updateData["alamat.city"] = tokoInput.Alamat.City
+	}
+	if tokoInput.Alamat.Description != "" {
+		updateData["alamat.description"] = tokoInput.Alamat.Description
+	}
+	if tokoInput.Alamat.PostalCode != "" {
+		updateData["alamat.postal_code"] = tokoInput.Alamat.PostalCode
+	}
+
+	// Jika menu ingin diperbarui
+	if tokoInput.Menu != nil {
+		updateData["menu"] = tokoInput.Menu
+	}
+
+	// Lakukan update ke database
+	update := bson.M{"$set": updateData}
+	_, err = config.Mongoconn.Collection("menu").UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error: Gagal mengupdate toko"
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusNotModified, respn)
+		return
+	}
+
+	// Buat response sukses
+	response := map[string]interface{}{
+		"status":  "success",
+		"message": "Toko berhasil diupdate",
+		"data":    tokoInput,
+	}
+
+	// Response sukses
+	at.WriteJSON(respw, http.StatusOK, response)
+}
+
 
 func GetPageMenuByToko(respw http.ResponseWriter, req *http.Request) {
 	// Ambil parameter slug dari query params, bukan URL params
