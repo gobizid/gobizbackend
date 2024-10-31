c620138d2bd30249944185238d6b7b6e0618de76
Ichika354 <handa82624@gmail.com>
2024-11-01 01:29:22 +0700

    add endpoint delete address

--- route/route.go
+++ route/route.go
@@ -217,6 +217,8 @@ func URL(w http.ResponseWriter, r *http.Request) {
 		controller.CreateAlamat(w, r)
 	case method == "PUT" && path == "/update/address":
 		controller.UpdateAlamat(w, r)
+	case method == "DELETE" && path == "/delete/address":
+		controller.DeleteAlamat(w, r)
 	case method == "GET" && path == "/get/address/province":
 		controller.GetAllProvinces(w, r)
 	case method == "GET" && path == "/get/address/city":
