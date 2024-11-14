23fbeb5536df65cf76b6655c745077812a635ea6
Ichika354 <handa82624@gmail.com>
2024-11-14 12:53:35 +0700

    Add get all user function

--- controller/profile.go
+++ controller/profile.go
@@ -50,3 +50,48 @@ func GetUserProfile(respw http.ResponseWriter, req *http.Request) {
 
 	at.WriteJSON(respw, http.StatusOK, response)
 }
+
+func GetAllUser(respw http.ResponseWriter, req *http.Request) {
+	tokenLogin := at.GetLoginFromHeader(req)
+	_, err := watoken.Decode(config.PublicKeyWhatsAuth, tokenLogin)
+	if err != nil {
+		_, err = watoken.Decode(config.PUBLICKEY, tokenLogin)
+		if err != nil {
+			var respn model.Response
+			respn.Status = "Error: Token tidak valid"
+			respn.Response = err.Error()
+			at.WriteJSON(respw, http.StatusForbidden, respn)
+			return
+		}
+	}
+
+	data, err := atdb.GetAllDoc[[]model.Userdomyikado](config.Mongoconn, "user", primitive.M{})
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error: User Tidak Ditemukan"
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusNotFound, respn)
+		return
+	}
+
+	if len(data) == 0 {
+		var respn model.Response
+		respn.Status = "Error: Data kategori kosong"
+		at.WriteJSON(respw, http.StatusNotFound, respn)
+		return
+	}
+
+	responseData := model.Response{
+		Status:   "Success",
+		Response: "Data pengguna berhasil diambil",
+		Info:     "Profil pengguna ditemukan",
+		Location: "GetAllUser",
+	}
+
+	response := map[string]interface{}{
+		"response": responseData,
+		"data":     data,
+	}
+
+	at.WriteJSON(respw, http.StatusOK, response)
+}
