02335bc2c420fcb4ca13a55e91b952171b055213
Ichika354 <handa82624@gmail.com>
2024-11-14 13:21:24 +0700

    Add function get user by id

--- controller/profile.go
+++ controller/profile.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gocroot/helper/atdb"
 	"github.com/gocroot/helper/watoken"
 	"github.com/gocroot/model"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -91,5 +92,54 @@ func GetAllUser(respw http.ResponseWriter, req *http.Request) {
 		"data":     data,
 	}
 
+	at.WriteJSON(respw, http.StatusOK, response)
+}
+
+func GetUserByID(respw http.ResponseWriter, req *http.Request) {
+	tokenLogin := at.GetLoginFromHeader(req)
+	_, err := watoken.Decode(config.PublicKeyWhatsAuth, tokenLogin)
+	if err != nil {
+		_, err = watoken.Decode(config.PUBLICKEY, tokenLogin)
+		if err != nil {
+			var respn model.Response
+			respn.Status = "Error: Token tidak valid"
+			respn.Response = err.Error()
+			at.WriteJSON(respw, http.StatusForbidden, respn)
+			return
+		}
+	}
+
+	UserID := req.URL.Query().Get("id")
+	if UserID == "" {
+		var respn model.Response
+		respn.Status = "Error"
+		respn.Response = "ID pengguna tidak ditemukan"
+		at.WriteJSON(respw, http.StatusNotFound, respn)
+		return
+	}
+
+	objectID, err := primitive.ObjectIDFromHex(UserID)
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error : ID pengguna tidak valid"
+		at.WriteJSON(respw, http.StatusBadRequest, respw)
+		return
+	}
+
+	var data model.Userdomyikado
+	filter := bson.M{"_id": objectID}
+	_, err = atdb.GetOneDoc[model.Userdomyikado](config.Mongoconn, "user", filter)
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error: User tidak ditemukan"
+		at.WriteJSON(respw, http.StatusNotFound, respw)
+		return
+	}
+
+	response := map[string]interface{}{
+		"status":  "success",
+		"message": "User ditemukan",
+		"data":    data,
+	}
 	at.WriteJSON(respw, http.StatusOK, response)
 }
