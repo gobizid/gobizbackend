3d5656fb50f1b7f63da5531e5743d4593378f89d
Ahmad Rifki Ayala <ayalarifki@gmail.com>
2024-11-15 08:56:45 +0700

    add connection database geo

--- config/db.go
+++ config/db.go
@@ -8,9 +8,18 @@ import (
 
 var MongoString string = os.Getenv("MONGOSTRING")
 
+var MongoStringGeo string = "mongodb+srv://ayalarifki:Edqt6j5LplXRs5OH@appabsensi.lnfmk5s.mongodb.net/"
+
 var mongoinfo = atdb.DBInfo{
 	DBString: MongoString,
 	DBName:   "jualin",
 }
 
 var Mongoconn, ErrorMongoconn = atdb.MongoConnect(mongoinfo)
+
+var MongoInfoGeo = atdb.DBInfo{
+	DBString: MongoStringGeo,
+	DBName:   "geo",
+}
+
+var MongoconnGeo, ErrorMongoconnGeo = atdb.MongoConnect(MongoInfoGeo)
