65617c83ff769c6cb1bfb70a2b2159c10c58d7d8
Ichika354 <handa82624@gmail.com>
2024-11-22 09:40:42 +0700

    add new data models for Region, Roads, and LongLat in menu.go

--- model/menu.go
+++ model/menu.go
@@ -41,6 +41,27 @@ type Toko struct {
 	User         []Userdomyikado    `bson:"user" json:"user"`
 }
 
+type Region struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Province    string             `bson:"province" json:"province"`
+	District    string             `bson:"district" json:"district"`
+	SubDistrict string             `bson:"sub_district" json:"sub_district"`
+	Village     string             `bson:"village" json:"village"`
+	Border      GeoJSONGeometry	           `bson:"border" json:"border"`
+}
+
+type Roads struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Type       string             `bson:"type" json:"type"`
+	Geometry   Geometry           `bson:"geometry" json:"geometry"`
+	Properties Properties         `bson:"properties" json:"properties"`
+}
+
+type LongLat struct {
+	Longitude float64 `bson:"long" json:"long"`
+	Latitude  float64 `bson:"lat" json:"lat"`
+}
+
 type GeoJSONFeature struct {
 	Type       string            `bson:"type" json:"type"`
 	Properties map[string]string `bson:"properties" json:"properties"`
